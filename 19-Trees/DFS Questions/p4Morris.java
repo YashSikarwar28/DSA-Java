d25756e6da1ca7272dba66e36d020cc8c69fbd51
Yash Sikarwar <yashsikarwar005@gmail.com>
2026-07-04 15:44:26 +0530

    Update p4Morris.java

--- 19-Trees/DFS Questions/p4Morris.java
+++ 19-Trees/DFS Questions/p4Morris.java
@@ -3,27 +3,29 @@
 //If a left node exist then go to the rightmost node of that left node and then connect the rightmost node with the root node and then move the left pointer to next left node
 //If left node does not exist go towards right
 
-public ArrayList<Integer> inOrder(Node root) {
-    ArrayList<Integer> a = new ArrayList<>();
-    Node t = root;
-    while (t != null) {
-        if (t.left == null) {
-            a.add(t.data);
-            t = t.right;
-        } else {
-            Node l = t.left;
-            while (l.right != null && l.right != t) {
-                l = l.right;
-            }
-            if (l.right == null) {
-                l.right = t;
-                t = t.left;
-            } else {
-                l.right = null;
-                a.add(t.data);
-                t = t.right;
+class Solution {
+    public ArrayList<Integer> inOrder(Node root) {
+        ArrayList<Integer> ans=new ArrayList<>();
+        Node curr=root;
+        while(curr!=null){
+            if(curr.left==null){
+                ans.add(curr.data);
+                curr=curr.right;
+            }else{
+                Node temp=curr.left;
+                while(temp.right!=null && temp.right!=curr){
+                    temp=temp.right;
+                }
+                if(temp.right==null){
+                    temp.right=curr;
+                    curr=curr.left;
+                }else{
+                    temp.right=null;
+                    ans.add(curr.data);
+                    curr=curr.right;
+                }
             }
         }
+        return ans;
     }
-    return a;
 }
