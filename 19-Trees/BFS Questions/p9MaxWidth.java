f52cccc1607339807eb1f3faf95c7bfccf08b968
Yash Sikarwar <yashsikarwar005@gmail.com>
2026-07-11 19:36:21 +0530

    Refactor widthOfBinaryTree method for clarity

--- 19-Trees/BFS Questions/p9MaxWidth.java
+++ 19-Trees/BFS Questions/p9MaxWidth.java
@@ -1,40 +1,36 @@
 //Go to each node and store index, its basically traversing + storing index of each node. Then subtract the 2 nodes which are at the maximum distance.
-//To prevent overflow we will take the min of i at each level and subtract the level from it.
 
 
 class Solution {
     class Pair{
         TreeNode node;
-        long num;
-        Pair(TreeNode node,long num){
+        long index;
+        Pair(TreeNode node,long index){
             this.node=node;
-            this.num=num;
+            this.index=index;
         }
     }
     public int widthOfBinaryTree(TreeNode root) {
-        if(root==null) return 0;
-        Queue<Pair> q=new LinkedList<>();
-        q.add(new Pair(root,0));
-        int ans=0;
-        while(!q.isEmpty()){
-            int size=q.size();
-            long min=q.peek().num;
-            long fir=0,last=0;
+        Deque<Pair> dq=new ArrayDeque<>();
+        dq.offerLast(new Pair(root,0));
+        int max=0;
+        while(!dq.isEmpty()){
+            int size=dq.size();
+            long left=dq.peekFirst().index;
+            long right=dq.peekLast().index;
+            max=Math.max(max,(int)(right-left+1));
             for(int i=0;i<size;i++){
-                long ind=q.peek().num-min;
-                TreeNode t=q.peek().node;
-                q.poll();
-                if(i==0) fir=ind;
-                if(i==size-1) last=ind;
-                if(t.left!=null){
-                    q.add(new Pair(t.left,ind*2+1));
+                Pair curr=dq.pollFirst();
+                TreeNode node=curr.node;
+                int ind=(int)curr.index;
+                if(node.left!=null){
+                    dq.offerLast(new Pair(node.left,2*ind+1));
                 }
-                if(t.right!=null){
-                    q.add(new Pair(t.right,ind*2+2));
+                if(node.right!=null){
+                    dq.offerLast(new Pair(node.right,2*ind+2));
                 }
             }
-            ans=Math.max(ans,(int)(last-fir+1));
         }
-        return ans;
+        return max;
     }
 }
