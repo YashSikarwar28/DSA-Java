2ffcf4691ac6956b455e510f4a25b22f5132ff21
Yash Sikarwar <yashsikarwar005@gmail.com>
2026-09-03 22:47:31 +0530

    Create 01-MaxPointsCard.java

---
+++ 06-Sliding Window/01-MaxPointsCard.java
@@ -0,0 +1,20 @@
+//Take k from one end and then take k from the other end and get the max.
+class Solution {
+    public int maxScore(int[] a, int k) {
+        int lsum=0;
+        int rsum=0;
+        int maxsum=0;
+        for(int i=0;i<=k-1;i++){
+            lsum+=a[i];
+        }
+        maxsum=lsum;
+        int rind=a.length-1;
+        for(int i=k-1;i>=0;i--){
+            lsum=lsum-a[i];
+            rsum=rsum+a[rind];
+            rind--;
+           maxsum=Math.max(maxsum,lsum+rsum);
+        }
+        return maxsum;
+    }
+}
