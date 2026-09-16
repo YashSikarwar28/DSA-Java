201f538156b6ef4cc67fd529172292a31d6daf63
Yash Sikarwar <yashsikarwar005@gmail.com>
2026-09-16 11:01:21 +0530

    Update 02-FloydWarshalLC1334.java

--- 03-Graphs/02-FloydWarshalLC1334.java
+++ 03-Graphs/02-FloydWarshalLC1334.java
@@ -44,76 +44,71 @@ class Solution {
 
 
 //The same question can be done using dijkstra only difference is that instead of checking distance only from source to destination we will this time check from every node to every other node in the graph
-ArrayList<ArrayList<Pair2>> al = new ArrayList<>();
+class Pair {
+    int j;
+    int dist;
 
-for (int i = 0; i < n; i++) {
-    al.add(new ArrayList<>());
+    Pair(int j, int dist) {
+        this.j = j;
+        this.dist = dist;
+    }
 }
 
-for (int i = 0; i < edges.length; i++) {
-    al.get(edges[i][0]).add(new Pair2(edges[i][1], edges[i][2]));
-    al.get(edges[i][1]).add(new Pair2(edges[i][0], edges[i][2]));
+class PQ {
+    int j;
+    int dist;
+
+    PQ(int j, int dist) {
+        this.j = j;
+        this.dist = dist;
+    }
 }
 
-int ans = -1;
-int minCount = Integer.MAX_VALUE;
-
-// Run Dijkstra from every node
-for (int src = 0; src < n; src++) {
-
-    PriorityQueue<Pair> pq =
-        new PriorityQueue<>((a, b) -> Integer.compare(a.dist, b.dist));
-
-    pq.add(new Pair(src, 0));
-
-    int[] dist = new int[n];
-    Arrays.fill(dist, Integer.MAX_VALUE);
-
-    dist[src] = 0;
-
-    while (!pq.isEmpty()) {
-
-        Pair pp = pq.poll();
-
-        int n1 = pp.node;
-        int d1 = pp.dist;
-
-        // Ignore outdated entry
-        if (d1 > dist[n1]) {
-            continue;
+class Solution {
+    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+        ArrayList<ArrayList<Pair>> al = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            al.add(new ArrayList<>());
         }
-
-        for (Pair2 p2 : al.get(n1)) {
-
-            int n2 = p2.j;
-            int d2 = p2.dist;
-
-            int newDist = d1 + d2;
-
-            if (newDist <= distanceThreshold && newDist < dist[n2]) {
-
-                dist[n2] = newDist;
-
-                pq.add(new Pair(n2, newDist));
+        int ans = -1;
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < edges.length; i++) {
+            al.get(edges[i][0]).add(new Pair(edges[i][1], edges[i][2]));
+            al.get(edges[i][1]).add(new Pair(edges[i][0], edges[i][2]));
+        }
+        //Running dijkstra on every node 
+        for (int i = 0; i < n; i++) {
+            PriorityQueue<PQ> pq = new PriorityQueue<>((a, b) -> Integer.compare(a.dist, b.dist));
+            pq.add(new PQ(i, 0));
+            int[] dist = new int[n];
+            Arrays.fill(dist, Integer.MAX_VALUE);
+            dist[i] = 0;
+            while (!pq.isEmpty()) {
+                PQ p = pq.poll();
+                int n1 = p.j;
+                int d1 = p.dist;
+                if (d1 > dist[n1])
+                    continue;
+                for (Pair p1 : al.get(n1)) {
+                    int n2 = p1.j;
+                    int d2 = p1.dist;
+                    if (d2 + d1 < dist[n2] && d2 + d1 <= distanceThreshold) {
+                        dist[n2] = d2 + d1;
+                        pq.add(new PQ(n2, d2 + d1));
+                    }
+                }
+            }
+            int c = 0;
+            for (int l = 0; l < n; l++) {
+                if (l != i && dist[l] <= distanceThreshold) {
+                    c++;
+                }
+            }
+            if (c <= min) {
+                min = c;
+                ans = i;
             }
         }
-    }
-
-    // Count cities reachable from src
-    int count = 0;
-
-    for (int i = 0; i < n; i++) {
-
-        if (i != src && dist[i] <= distanceThreshold) {
-            count++;
-        }
-    }
-
-    // If same count, choose larger city number
-    if (count <= minCount) {
-        minCount = count;
-        ans = src;
+        return ans;
     }
 }
-
-return ans;
